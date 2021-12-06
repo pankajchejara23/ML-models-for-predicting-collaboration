4f7bff99155992fa1524cf4f7d3eb5d78773398e
Pankaj Chejara <pankajchejara23@gmail.com>
2021-12-06 23:37:03 +0200

    add head angle estimation

--- extractFacePoseHand.py
+++ extractFacePoseHand.py
@@ -4,7 +4,8 @@ import mediapipe as mp
 import math
 import pandas as pd
 from typing import List, Mapping, Optional, Tuple, Union
-
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 mp_holistic = mp.solutions.holistic
 
@@ -12,13 +13,114 @@ mp_holistic = mp.solutions.holistic
 # idea is taken from https://github.com/google/mediapipe/blob/master/mediapipe/python/solutions/drawing_utils.py
 _PRESENCE_THRESHOLD = 0.5
 _VISIBILITY_THRESHOLD = 0.5
+font = cv2.FONT_HERSHEY_SIMPLEX
 
+def draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix, color=(255, 255, 0), line_width=2):
+    """Draw a 3D box as annotation of pose"""
+    point_3d = []
+    dist_coeffs = np.zeros((4,1))
+    front_size = img.shape[1]/4
+    front_depth = img.shape[1]/4
+    rear_size = img.shape[1]/6
+    rear_depth = 0
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d.append((-front_size, front_size, front_depth))
+    point_3d.append((front_size, front_size, front_depth))
+    point_3d.append((front_size, -front_size, front_depth))
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
+
+    # Map to 2d img points
+    (point_2d, _) = cv2.projectPoints(point_3d,
+                                      rotation_vector,
+                                      translation_vector,
+                                      camera_matrix,
+                                      dist_coeffs)
+    point_2d = np.int32(point_2d.reshape(-1, 2))
+    # # Draw all the lines
+    cv2.polylines(img, [point_2d], True, color, line_width, cv2.LINE_AA)
+    k = (point_2d[5] + point_2d[8])//2
+    cv2.line(img, tuple(point_2d[1]), tuple(
+        point_2d[6]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[2]), tuple(
+        point_2d[7]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[3]), tuple(
+        point_2d[8]), color, line_width, cv2.LINE_AA)
+
+    return(point_2d[2], k)
+
+def estimateHeadPose(shape,size,image1):
+    def _normalized_to_pixel_coordinates1(
+        normalized, image_width: int,
+        image_height: int) -> Union[None, Tuple[int, int]]:
+        """Converts normalized value pair to pixel coordinates."""
+        # Checks if the float value is between 0 and 1.
+        def is_valid_normalized_value(value: float) -> bool:
+            return (value > 0 or math.isclose(0, value)) and (value < 1 or
+                                                          math.isclose(1, value))
+
+        if not (is_valid_normalized_value(normalized.x) and
+              is_valid_normalized_value(normalized.y)):
+            # TODO: Draw coordinates even if it's outside of the image bounds.
+            return None
+        x_px = min(math.floor(normalized.x * image_width), image_width - 1)
+        y_px = min(math.floor(normalized.y * image_height), image_height - 1)
+        z_px = min(math.floor(normalized.z * image_height), image_height - 1)
+        return x_px, y_px
+
+    image_rows, image_cols, _ = size
+
+    focal_length = size[1]
+    center = (size[1]/2, size[0]/2)
+    camera_matrix = np.array(
+             [[focal_length, 0, center[0]],
+             [0, focal_length, center[1]],
+             [0, 0, 1]], dtype = "double")
+    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+
+    model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip
+                            (0.0, -330.0, -65.0),        # Chin
+                            (-225.0, 170.0, -135.0),     # Left eye left corner
+                            (225.0, 170.0, -135.0),      # Right eye right corne
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner
+                            (150.0, -150.0, -125.0)      # Right mouth corner
+                        ])
+
+    image_points = np.array([
+                            _normalized_to_pixel_coordinates1(shape[1], image_cols, image_rows),     # Nose tip
+                            _normalized_to_pixel_coordinates1(shape[152], image_cols, image_rows),     # Chin
+                            _normalized_to_pixel_coordinates1(shape[226], image_cols, image_rows),     # Left eye left corner
+                            _normalized_to_pixel_coordinates1(shape[446], image_cols, image_rows),     # Right eye right corne
+                            _normalized_to_pixel_coordinates1(shape[57], image_cols, image_rows),     # Left Mouth corner
+                            _normalized_to_pixel_coordinates1(shape[287], image_cols, image_rows)      # Right mouth corner
+                        ], dtype="double")
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+    # Rotation from r
+    r = Rotation.from_rotvec(rotation_vector.reshape(3,))
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+    p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+    p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+    x1, x2 = draw_annotation_box(image1, rotation_vector, translation_vector, camera_matrix)
+
+    angles = r.as_euler('zxy',degrees=True)
+    print(" ")
+    x_angle = int(angles[1])
+    y_angle  = int(angles[2])
+    z_angle = int(angles[0])
+    return x_angle,y_angle,z_angle
 
 # this function is also taken from https://github.com/google/mediapipe/blob/master/mediapipe/python/solutions/drawing_utils.py
 # As MediaPipe return landmarks in normalized form, we need to convert them back relative to the image.
 def _normalized_to_pixel_coordinates(
     normalized_x: float, normalized_y: float,normalized_z: float, image_width: int,
-    image_height: int) -> Union[None, Tuple[int, int]]:
+    image_height: int) -> Union[None, Tuple[int, int, int]]:
     """Converts normalized value pair to pixel coordinates."""
     # Checks if the float value is between 0 and 1.
     def is_valid_normalized_value(value: float) -> bool:
@@ -39,8 +141,6 @@ def extractMediaPipeFeatures(video_file,draw=False,save=False):
     cap = cv2.VideoCapture(video_file)
     # columns of the output csv file
     face_pose_landmarks_columns = []
-
-
     # there are 468 face landmarks  returned by MediaPipe
     for i in range(468):
         x = 'face_' + str(i)+'_x'
@@ -57,6 +157,10 @@ def extractMediaPipeFeatures(video_file,draw=False,save=False):
         face_pose_landmarks_columns.append(x)
         face_pose_landmarks_columns.append(y)
         face_pose_landmarks_columns.append(z)
+
+    face_pose_landmarks_columns.append('pose_angle_x')
+    face_pose_landmarks_columns.append('pose_angle_y')
+    face_pose_landmarks_columns.append('pose_angle_z')
 
     output_df = pd.DataFrame(columns=face_pose_landmarks_columns)
     # for labeling resultant image
@@ -81,8 +185,17 @@ def extractMediaPipeFeatures(video_file,draw=False,save=False):
             image.flags.writeable = True
             idx_to_coordinates = {}
 
-            print('Face coordinates:',len(results.face_landmarks.landmark))
-            print('Pose coordinates:',len(results.pose_landmarks.landmark))
+            if not  results.face_landmarks:
+                continue
+
+            facial_landmarks = results.face_landmarks.landmark
+            x1,y1,z1 = estimateHeadPose(facial_landmarks,image.shape,image)
+            cv2.putText(image, 'X:'+str(x1), (10,30), font, 2, (255, 255, 128), 3)
+            cv2.putText(image, 'Y:'+str(y1), (10,80), font, 2, (255, 255, 128), 3)
+
+            idx_to_coordinates['pose_angle_x'] = x1
+            idx_to_coordinates['pose_angle_y'] = y1
+            idx_to_coordinates['pose_angle_z'] = z1
 
 
             for idx, landmark in enumerate(results.face_landmarks.landmark):
@@ -135,10 +248,11 @@ def extractMediaPipeFeatures(video_file,draw=False,save=False):
                 fname = 'img_' + str(i) + '.jpg'
                 cv2.imwrite(fname,image)
 
-            print('Processing frame:',i)
+
             i = i + 1
-            print('Frame:',i)
-            if i == 20:
+
+            cv2.imshow('preview',image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     cap.release()
     return output_df
