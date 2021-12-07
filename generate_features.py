fd9145f7147a765e2864e09cdd12641cc217929d
Pankaj Chejara <pankajchejara23@gmail.com>
2021-12-07 12:05:17 +0200

    add generation of log, vad, speech file in sync manner with starting time as zero

--- generate_features.py
+++ generate_features.py
@@ -5,7 +5,16 @@ import datetime
 import pandas as pd
 import sys
 
-def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,window,group,output_file_name,mapping_file):
+def recomputeTimestamp(data,start_time):
+    co = data.drop_duplicates(ignore_index=False)
+    co.timestamp = pd.to_datetime(co['timestamp'])
+    co = co.reset_index()
+    co.timestamp = co.timestamp - start_time
+    co.timestamp = pd.Series([val.total_seconds() for val in co.timestamp])
+    co = co.reset_index()
+    return co
+
+def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,window,group,output_file_name,mapping_file,prefix):
     """
     This script takes three files etherpad logs, vad, speech from CoTrack tool and
     perform feature extraction and feature level fusion.
@@ -56,7 +65,7 @@ def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,windo
         record = mapping.iloc[i,:]
         user_mapping[record['authorid']] = record['username']
 
-    
+
 
 
     #changing datatype of timestamp column
@@ -71,6 +80,26 @@ def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,windo
 
     vad['timestamp'] = vad['timestamp'].dt.tz_localize(None)
     speech['timestamp'] = speech['timestamp'].dt.tz_localize(None)
+
+    # vad, speech, log files after sync
+    sync_log_mask = (log['timestamp'] > start_time) & (log['timestamp'] <= end_time)
+    sync_vad_mask = (vad['timestamp'] > start_time) & (vad['timestamp'] <= end_time)
+    sync_speech_mask = (speech['timestamp'] > start_time) & (speech['timestamp'] <= end_time)
+
+    sync_log = recomputeTimestamp(log[sync_log_mask],start_time)
+    sync_vad = recomputeTimestamp(vad[sync_vad_mask],start_time)
+    sync_speech = recomputeTimestamp(speech[sync_speech_mask],start_time)
+
+
+
+
+    sync_log_name = prefix + '_group_' + str(group) + '_sync_log_features.csv'
+    sync_vad_name = prefix + '_group_' + str(group) + '_sync_vad_features.csv'
+    sync_speech_name = prefix + '_group_' + str(group) + '_sync_speech_features.csv'
+
+    sync_log.to_csv(sync_log_name,index=False)
+    sync_vad.to_csv(sync_vad_name,index=False)
+    sync_speech.to_csv(sync_speech_name,index=False)
 
     frame_start = start_time
 
@@ -91,62 +120,47 @@ def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,windo
         frame_end = frame_start + window
         #print('frame start:',frame_start,'frame_end:',frame_end)
         print('Frame:',frame_no)
-
         # fetching data records for the specified window
         mask_log = (log['timestamp'] > frame_start) & (log['timestamp'] <= frame_end)
         mask_vad = (vad['timestamp'] > frame_start) & (vad['timestamp'] <= frame_end)
         mask_speech = (speech['timestamp'] > frame_start) & (speech['timestamp'] <= frame_end)
-
         df = log[mask_log]
         df_vad = vad[mask_vad]
         df_speech = speech[mask_speech]
 
 
-        sequence = df_speech['user'].to_list()
+        df_vad = df_vad.drop_duplicates(ignore_index=True)
+        df_speech = df_speech.drop_duplicates(ignore_index=True)
 
+        sequence = df_vad['user'].to_list()
         # For computing turn-taking
         turn_df = pd.DataFrame(columns=['label','conti_frequency'])
-
         # This function will count the number of continuous occurence
         def count_conti_occurence(index):
-
             # Set count to 0
             count=0
-
             # Starts from the given index
             j = index
-
             # Loop to iterate over the users sequence
             while j<len(sequence):
-
                 # Increase the count if the element at given index (parameter) is same as the iterated element
                 if sequence[j] == sequence[index]:
                     count +=1
-
                 # If mismatch found, break the loop
                 else:
                     break
-
                 # Increases j
                 j +=1
-
             # Return number of count for sequence[index] and index of first next occurence of different element.
             return count,(j-index)
-
         # Set i to 0 for the Loop
         i = 0
-
         # Iterate for entire sequence of users
         while i < len(sequence):
-
             # Call count_conti_occurence() function
             count,diff = count_conti_occurence(i)
-
-
             # Add continuous frequency of current user (sequence[i]) to the dataframe
             turn_df = turn_df.append({'label':sequence[i],'conti_frequency':count},ignore_index=True)
-
-
             # Move to next different element
             i = i + diff
 
@@ -162,8 +176,7 @@ def feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,windo
             author_vad_df = df_vad.loc[df_vad['user'] == author,:]
             author_speech_df = df_speech.loc[df_speech['user'] == author,:]
 
-
-
+            
 
 
             if author_df.shape[0] != 0:
@@ -223,47 +236,28 @@ def generateFeatures(video_dir,log_file,vad_file,speech_file,window,output_file_
             #f_name = video_dir + "/" + f
             f_name = f
             dirlist[group].append(f_name)
-
-    # extract timestamp from video file names
-
-
+    # extract timestamp from video file name
     for key in dirlist.keys():
         print('Group:',key)
         print('============================')
         duration = []
         files = dirlist[key]
-
         ts = [int(f.split(".")[0].split("_")[5]) for f in files]
-
         users = [int(f.split(".")[0].split("_")[2]) for f in files]
-
         duration = [VideoFileClip(video_dir + "/" + vid).duration for vid in files ]
-
         ts_start = [(t/1000-duration[i]) for i,t in enumerate(ts)]
-
         ts_end = [t/1000  for t in ts]
-
         ts_end_diff = [ ( t - min(ts_end))/1000 for t in ts_end]
-
         ts_start_diff_tmp = [ max(ts_start) - t for t in ts_start]
-
         ts_start_diff  = [t   for t in ts_start_diff_tmp]
-
-
         start_time = datetime.datetime.fromtimestamp(int(max(ts_start)))
         end_time = datetime.datetime.fromtimestamp(int(max(ts_end)))
-
-
         output_file_name = output_file_prefix + '_group_' + str(key) + '.csv'
-
         print('start:',start_time,'end_time:',end_time)
         #dddd = feature_level_fusion('BT19_logs.csv','BT19_vad.csv','BT19_speech.csv',start_time,end_time,window,1,'demo.csv','BT19_mapping.csv')
-        dddd = feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,window,1,output_file_name,mapping_file)
-
+        dddd = feature_level_fusion(log_file,vad_file,speech_file,start_time,end_time,window,key,output_file_name,mapping_file,output_file_prefix)
         dddd.to_csv(output_file_name,index=False)
         print('Generated features for group-',group,' save in file:',output_file_name)
-
-
 if len(sys.argv) < 7:
     print('Incorrect use, please use the script in following way.\n')
     print("Use this format: \n python create_split_screen.py <video_directory_path> <log_file> <vad_file> <speech_file> <window:30 seconds> <prefix> <mapping_file> ")
@@ -274,9 +268,9 @@ if len(sys.argv) < 7:
     print('        <window> is the time window.')
     print('        <prefix> is the prefix for output files.')
     print('        <mapping_file> is the mapping file from CoTrack.')
-
     exit()
 else:
+    print(sys.argv)
     video_dir = sys.argv[1]
     log_file = sys.argv[2]
     vad_file = sys.argv[3]
